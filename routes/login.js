262d3b7f5eda756413511ed087ee2879d5adbba6
Kan <kanz.kingz@gmail.com>
2019-11-25 09:05:44 +0700

    kan 9.05

--- routes/login.js
+++ routes/login.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 const crypto = require('crypto');
 const User = require("../models/Sequelize");
+var passport = require('passport');
 const app = express();
-
 router.route('/')
     .all((req, res, next) => {
         res.locals.pageData = {
@@ -15,49 +15,44 @@ router.route('/')
         res.render('pages/login')
     })
     .post((req, res, next) => {
-
         const bodyParser = require('body-parser');
-
         // parse application/json
         app.use(bodyParser.json());
         app.use(bodyParser.urlencoded({
             extended: true
         }));
-
         const usersec = req.body.user
-        opass = req.body.pass
-            ;
-
+        opass = req.body.pass;
         User.findAll({
-            where: {
-                user: usersec
-            }
+            where: { user: usersec }
         }).then(result => {
             result.forEach(function (data) {
-
                 //console.log(data.pass);
-
                 var mykey = crypto.createDecipher('aes-128-cbc', 'abc');
                 var mystr = mykey.update(data.pass, 'hex', 'utf8')
                 mystr += mykey.final('utf8');
-                
 
-                if (!mystr == opass) {
-                    console.log("Login don't Successfully");
-                    res.redirect('/login');
-                }
-                else {
-                    console.log("Login Successfully");
-                    var username = data.user;
-                    res.redirect('/dashboard?user=' + username);
-                };
+                app.get('/auth/example', passport.authenticate('oauth2'));
 
+                app.get('/auth/example/callback',
+                    passport.authenticate('oauth2', { failureRedirect: '/login' }),
+                    function (req, res) {
+                        // Successful authentication, redirect home.
+                        res.redirect('/');
+                    });
 
+                // if (!mystr == opass) {
+                //     console.log("Login don't Successfully");
+                //     res.redirect('/login');
+                // }
+                // else {
+                //     console.log("Login Successfully");
+                //     var username = data.user;
+                //     res.redirect('/dashboard?user=' + username);
+                // };
             });
         });
-
         console.log();
-
         // const conn = require('../connection/con');
         // const user = req.body.user
         //       opass = req.body.pass
@@ -66,25 +61,19 @@ router.route('/')
         // const sql = "SELECT * FROM user WHERE user = '" + user + "'";
         // console.log("sql is =" + sql);
         // conn.query(sql, function (err, result) {
-
         //     result.forEach(function (data) {
-
         //         var mykey = crypto.createDecipher('aes-128-cbc', 'abc');
         //         var mystr = mykey.update(data.pass, 'hex', 'utf8')
         //         mystr += mykey.final('utf8');
-
         //         if (!mystr == opass) {
         //             console.log("Login don't Successfully");
         //             res.redirect('/login');
-        //         }
-        //         else {
+        //         } else {
         //             console.log("Login Successfully");
         //             var username = data.user ;
         //             res.redirect('/dashboard?user=' + username);
         //         };
-
         //     });
-
         // });
     });
 
